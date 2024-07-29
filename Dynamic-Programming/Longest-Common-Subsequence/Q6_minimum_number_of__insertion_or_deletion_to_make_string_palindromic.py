5edd7d1faf6915f951cf757f6ed07f39c34ab981
shivambhilarkar <shivambhilarkar@gmail.com>
2024-07-29 08:28:37 +0530

    Dynamic Programming Questions

--- Dynamic-Programming/Longest-Common-Subsequence/Q6_minimum_number_of__insertion_or_deletion_to_make_string_palindromic.py
+++ Dynamic-Programming/Longest-Common-Subsequence/Q6_minimum_number_of__insertion_or_deletion_to_make_string_palindromic.py
@@ -1,9 +1,7 @@
-# minimum number of deletion and insertion to convert string a to b
-# this question is extension of longest common subsequence
-# https://leetcode.com/problems/delete-operation-for-two-strings/description/
+# minimum number of insertion to make string a to b
+# minimum number of deletion to make string a to b
 
-class Solution:
-
+class solution:
     def tabulation_lcs(self, text1: str, text2: str) -> int:
         dp = [[0 for _ in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
         for i in range(1, len(dp)):
@@ -14,11 +12,15 @@ class Solution:
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
         return dp[len(text1)][len(text2)]
 
-    def tabulation_solution(self, text1, text2) -> int:
+    def tabulation_minimum_no_of_insertion(self, text1: str, text2: str) -> int:
         lcs = self.tabulation_lcs(text1, text2)
-        deletion = len(text1) - lcs
-        insertion = len(text2) - lcs
-        return deletion + insertion
+        remaining_character = lcs - len(text1)
+        return remaining_character
+
+    def tabulation_minimum_no_of_deletion(self, text1: str, text2: str) -> int:
+        lcs = self.tabulation_lcs(text1, text2)
+        remaining_character = lcs - len(text1)
+        return remaining_character
 
 
 if __name__ == '__main__':
